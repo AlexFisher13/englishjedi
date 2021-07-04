f634c1a3a07aef854bb39d61f4f2c132e5c8f7a6
Fisher <alexfisher13@yandex.ru>
2021-07-04 15:00:02 +0300

    init

---
+++ js/menu.js
@@ -0,0 +1,17 @@
+document.write(
+    "<header><a href=\"index.html\"><h1 id=\"jedi\">EnglishJedi</h1></a></header>" +
+    "<nav class='pushy pushy-left' data-focus='#first-link'>\n" +
+    "    <div class='pushy-content'>\n" +
+    "        <ul>\n" +
+    "            <li class='pushy-submenu'>\n" +
+    "                <button>Tenses</button>\n" +
+    "                <ul>\n" +
+    "                    <li class='pushy-link'><a href='/englishjedi/pages/present.html'>Present</a></li>\n" +
+    "                    <li class='pushy-link'><a href='/englishjedi/pages/past.html'>Past</a></li>\n" +
+    "                    <li class='pushy-link'><a href='/englishjedi/pages/future.html'>Future</a></li>\n" +
+    "                </ul>\n" +
+    "            </li>\n" +
+    "            <li class='pushy-link'><a href='/englishjedi/pages/modal.html'>Modal verbs</a></li>\n" +
+    "        </ul>\n" +
+    "    </div>\n" +
+    "</nav>")
